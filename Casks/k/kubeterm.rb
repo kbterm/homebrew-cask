d485a3daf416786b13170f602de7230c1543987e
Dai Ming <daiming@outlook.com>
2025-08-13 15:48:21 +0800

    Add kubernetes gui tool kubeterm v1.6.2

---
+++ Casks/k/kubeterm.rb
@@ -0,0 +1,28 @@
+cask "kubeterm" do
+  version "1.6.2"
+  sha256 "9b5f7e67b01f47d495c207055cf52e69644e214ed8db653933de3b9621f6d4fa"
+
+  url "https://github.com/kbterm/kubeterm/releases/download/v#{version}/Kubeterm-v#{version}-universal.dmg",
+      verified: "github.com/kbterm/kubeterm/"
+  name "Kubeterm"
+  desc "Kubernetes graphical management tool"
+  homepage "https://www.kubeterm.com/"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  depends_on macos: ">= :monterey"
+
+  app "kubeterm.app"
+
+  zap trash: [
+    "~/Library/Application Support/com.kubeterm",
+    "~/Library/Caches/com.kubeterm",
+    "~/Library/Logs/com.kubeterm",
+    "~/Library/Preferences/com.kubeterm.plist",
+    "~/Library/Saved Application State/com.kubeterm.savedState",
+  ]
+end
+
